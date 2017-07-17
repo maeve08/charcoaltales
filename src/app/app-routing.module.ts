6e1db0c82d1420dd00cac49142b7f1054258cc68
Eva Plasencia <maeve08@gmail.com>
2017-07-17 22:40:46 +1000

    Code cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ViewStoriesComponent } from './view-stories/view-stories.component';
 import { StoryComponent } from './story/story.component';
 import { ShareStoryComponent } from './share-story/share-story.component';
@@ -12,8 +10,6 @@ import { FooterComponent } from './footer/footer.component';
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'detail/:id', component: HeroDetailComponent},
-  {path: 'heroes', component: HeroesComponent},
   {path: 'view-stories', component: ViewStoriesComponent},
   {path: 'story', component: StoryComponent},
   {path: 'share-story', component: ShareStoryComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
-import {InMemoryDataService} from './in-memory-data.service';
-
-import { StarRatingModule } from 'angular-star-rating';
 import { HeaderMainComponent } from './header-main/header-main.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderContentComponent } from './header-content/header-content.component';
@@ -28,9 +20,6 @@ import { ShareStoryComponent } from './share-story/share-story.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    HeroSearchComponent,
     HeaderMainComponent,
     FooterComponent,
     HeaderContentComponent,
@@ -42,11 +31,9 @@ import { ShareStoryComponent } from './share-story/share-story.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule,
-    StarRatingModule.forRoot()
   ],
-  providers: [HeroService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
